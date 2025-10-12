4461579f6b504f4268ed16e1a13ea501aaaafc3b
plazzonina-bit <plazzonina@gmail.com>
2025-10-12 16:08:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ st.set_page_config(page_title="Clarity")
 import google.generativeai as genai
 
 # Pega tu clave de API aquí.
-GOOGLE_API_KEY = "AIzaSyDEGUJBCiaXwgMyH_9_Az79yNFUjbAJR1w"
-genai.configure(api_key=GOOGLE_API_KEY)
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 # Define el "prompt" o instrucción para el modelo de IA.
 # Usamos f-strings para insertar las variables directamente en el texto.
@@ -70,6 +69,7 @@ if st.button("Analizar Campaña"):
     else:
 
         st.warning("Por favor, completa todos los campos para el análisis.")
+
 
 
 
